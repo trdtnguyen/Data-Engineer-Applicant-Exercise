a54725b91695567b331c476844ad7c6c16852f46
trdtnguyen <trauconnguyen@gmail.com>
2021-03-02 16:02:07 -0600

    finished simple_refactoring

--- simple_refactoring_exercise/python/template.py
+++ simple_refactoring_exercise/python/template.py
@@ -1,21 +1,38 @@
 def template(source_template, req_id):
-    
+    # refactoring: use const
+    split_pattern1 = "%CODE%"
+    split_pattern2 = "%ALTCODE%"
     template = str(source_template)
 
+    #refactoring: validations
+    if len(req_id) != 10:
+        print('invalid input')
+        return None
+    if split_pattern1 not in source_template or split_pattern2 not in source_template:
+        print('invalid input')
+        return None
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
+    # refactoring: use const instead of fixed value
+    template_split_begin = template.index(split_pattern1)
+    # refactoring: use len() instead of fixed value
+    #template_split_end = template_split_begin + 6
+    template_split_end = template_split_begin + len(split_pattern1)
+
     template_part_one = str(template[0:(template_split_begin)])
     template_part_two = str(template[template_split_end:len(template)])
     code = str(req_id)
     template = str(template_part_one + code + template_part_two)
 
     # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
+    # refactoring: use const instead of fixed value
+    #template_split_begin = template.index("%ALTCODE%")
+    template_split_begin = template.index(split_pattern2)
+    # refactoring: use len() instead of fixed value
+    #template_split_end = template_split_begin + 9
+    template_split_end = template_split_begin + len(template_part_two)
     template_part_one = str(template[0:(template_split_begin)])
     template_part_two = str(template[template_split_end:len(template)])
     altcode = code[0:5] + "-" + code[5:8]
     return template_part_one + altcode + template_part_two
-  
+
 
--- simple_refactoring_exercise/python/template_test.py
+++ simple_refactoring_exercise/python/template_test.py
@@ -1,14 +1,24 @@
 import unittest
 from template import template
- 
+
 class TestTemplate(unittest.TestCase):
- 
+
     def setUp(self):
         pass
- 
+
     def test_substitute_code_and_altcode(self):
         self.assertEqual( template('Code is %CODE%; alt code is %ALTCODE%', '5678901234'), 'Code is 5678901234; alt code is 56789-012')
- 
- 
+
+    def test_validate_req_id(self):
+        self.assertEqual(template('Code is %CODE%; alt code is %ALTCODE%', '1234'), None, 'req_id should be has len of 10')
+
+    def test_validate_code(self):
+        self.assertEqual(template('Code is %COD%; alt code is %ALTCODE%', '1234567891'), None, '%CODE% must appear in source_template')
+
+    def test_validate_altcode(self):
+        self.assertEqual(template('Code is %CODE%; alt code is %ACODE%', '1234567891'), None, '%ALTCODE% must appear in source_template')
+
+
+
 if __name__ == '__main__':
     unittest.main()
